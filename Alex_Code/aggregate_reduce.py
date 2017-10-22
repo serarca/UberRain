d334cb99061f18e303a08944afd909faf6c7d2e6
Alex Paramo <aparamo@rice10.stanford.edu>
2017-10-22 14:21:11 -0700

    updated step  5

--- Alex_Code/aggregate_reduce.py
+++ Alex_Code/aggregate_reduce.py
@@ -10,36 +10,27 @@ out_header = ['date', 'hour', 'drivers_onduty' 't_onduty', 't_occupied', 'n_pass
 last_key = None
 master_array = []	
 key = ""
-counter = 1
+driver_count = 1
 # formates array
+
 def format_line(line):
 	formatted = []
 	for i in range(len(in_header)):
-		if (i >= 3):
+		if (i >= 4):
 			try: 
 				value = int(line.pop(0))
 			except ValueError:
 				pass
-				
 		else:
 			value = line.pop(0)
 		formatted.append(value)
 	return formatted
 
-# Aggregates line 
-#def process(master_array, array):
-#	global master_array
-#	if (len(master_array) == 0):
-#		master_array = array
-#	else:
-#		for i in range(3, len(in_header)):
-#			master_array[i] += array[i]		 
-#	return array
 
-# streaming file line by line	
+# streaming
 for line in in_file:
-#	global master_array
 	key = line[0]
+
 	# formats line
 	line = format_line(line)
 	
@@ -47,16 +38,20 @@ for line in in_file:
 	if not last_key:
 		last_key = key
 		master_array = line
-
+	
+	# if key match
 	if (key == last_key):
+		driver_count += 1
+
+		# aggregate
 		for i in range(3, len(in_header)):
 			master_array[i] += line[i]
-#		master_array = process(master_array, line)
-		print("TINDER MATCH")	
-		print(master_array)
-
 	else:
-		print("FINAL GROUP")
-		print(master_array)
+		line1 = ('{0},{1},{2}'.format(master_array[1], master_array[2], str(driver_count)))
+		line2 =	(','.join(str(master_array[i]) for i in range(4, len(master_array))))
+		print(line1 + "," + line2)
 		master_array = line	
 		last_key = key
+		driver_count = 1
+# output header
+print(','.join(out_header[i] for i in range(len(out_header))))
