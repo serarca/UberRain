1e2363afa9da759292228a247fd8aea9ee8131fe
Alex Paramo <aparamo@rice13.stanford.edu>
2017-10-23 11:51:10 -0700

    step 5 and 6

--- Alex_Code/aggregate_reduce.py
+++ Alex_Code/aggregate_reduce.py
@@ -4,6 +4,7 @@ import sys
 import csv
 
 
+
 in_file = csv.reader(sys.stdin, delimiter=',')
 
 # headers
@@ -16,15 +17,12 @@ master_array = []
 key = ""
 driver_count = 1
 
-# formates array
+# formates array, converts values from str to floats for indices 4:10
 def format_line(line):
 	formatted = []
 	for i in range(len(in_header)):
-		if (i >= 4):
-			try: 
-				value = int(line.pop(0))
-			except ValueError:
-				pass
+		if(i >= 4):
+			value = float(line.pop(0))
 		else:
 			value = line.pop(0)
 		formatted.append(value)
@@ -44,18 +42,18 @@ for line in in_file:
 	
 	# if key match
 	if (key == last_key):
-		driver_count += 1
+		driver_count += 1 #adds driver
 
 		# aggregate
-		for i in range(3, len(in_header)):
+		for i in range(4 , len(in_header)):
 			master_array[i] += line[i]
-	# if key doesn't match 
+	# if new group 
 	else:
+		# string values
 		line1 = ('{0},{1},{2}'.format(master_array[1], master_array[2], str(driver_count)))
+		# float values
 		line2 =	(','.join(str(master_array[i]) for i in range(4, len(master_array))))
 		print(line1 + "," + line2)
 		master_array = line	
 		last_key = key
-		driver_count = 1
-# output header
-print(','.join(out_header[i] for i in range(len(out_header))))
+		driver_count = 1 #reset driver count
--- Alex_Code/join_percip.R
+++ Alex_Code/join_percip.R
@@ -1,22 +1,38 @@
 #Joins NOAA percipitation data with taxi trip data
+
 #Set up and initialization of packages
 library('tidyverse')
+
+# read in data
 percip_data <- read.csv(file="nyc_precipitation.csv", header=TRUE, sep=",")
-tax_data <- read.csv(file="join_test2.csv", header=TRUE, sep=",")
+taxi_data <- read.csv(file="test_step6.csv", header=FALSE, sep=",")
 
-#Prepares data for merge
-split_dt = data.frame(data=percip_data)
+# set header
+colnames(taxi_data) <- c("date", "hour", "drivers_onduty", "t_onduty", "t_occupied","n_pass", "n_trip", "n_mile", "earnings")
 
-split_dt <- separate(data=split_dt, col='data.DATE', into=c("date", "time"), sep=" ")
-split_dt$date <- (split_dt$date)
-split_dt$time <- as.numeric(split_dt$time)
-split_dt$date <- as.Date(split_dt$date, format="%Y%m%d")
-split_dt$date
-
+#Prepares percipitation data for merge
+split_dt = data.frame(data=percip_data) 
+split_dt$data.DATE = strptime(data=split_dt$data.DATE, format = '%Y%m%d %H:%M')
+split_dt$data.DATE
 
 
-#taxi_percip_data <- left_join(x=tax_data, y=percip_data)
 
-#print(taxi_percip_data)
+#split_dt$time <- strptime(x = split_dt$time, format="%H:%M")
+#split_dt$time <- as.numeric(split_dt$time, na.rm=TRUE) #not working
+#split_dt$time
+#split_dt$date <- as.Date(split_dt$date, format="%Y%m%d") # works
+
+# Prepares taxi data for merge
+formatted_taxi <- data.frame(data = taxi_data)
+
+
+#formatted_taxi$date <- as.Date(formatted_taxi$date, format="%m/%d/%Y")
+#formatted_taxi$hour <- as.numeric(formatted_taxi$hour)
+#formatted_taxi$date
+
+
+#Merge
+#taxi_percip_data <- left_join(x=formatted_taxi, y=split_dt)
+
  
 
