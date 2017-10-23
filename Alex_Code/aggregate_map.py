ce3d65879e1856b354c2eceef580ed9a18583f9b
Alex Paramo <aparamo@rice10.stanford.edu>
2017-10-23 08:47:17 -0700

    step 5

--- Alex_Code/aggregate_map.py
+++ Alex_Code/aggregate_map.py
@@ -8,8 +8,6 @@ in_file = csv.reader(sys.stdin, delimiter=',')
 header = ['date', 'hour', 'hack', 't_onduty', 't_occupied', 'n_pass', 'n_trip', 'n_mile', 'earnings']
 
 for line in in_file:
-#	print(line)
-#	print(len(line))
 	dict = {header[i]:line[i] for i in range(len(line))}
 	key = dict['date'] + dict['hour']
 	print (key + ',' + ','.join(line))
