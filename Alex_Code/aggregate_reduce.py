2a7e1caa17188f77a634a5cea1f96292d8aee009
pyrobluestar <32757427+pyrobluestar@users.noreply.github.com>
2017-10-24 15:46:58 -0700

    Add files via upload

--- Alex_Code/aggregate_reduce.py
+++ Alex_Code/aggregate_reduce.py
@@ -4,13 +4,13 @@ import sys
 
 # headers
 in_header = ['key', 'date', 'hour', 'hack', 't_onduty', 't_occupied', 'n_pass', 'n_trip', 'n_mile', 'earnings']
-out_header = ['date', 'hour', 'drivers_onduty' 't_onduty', 't_occupied', 'n_pass', 'n_trip', 'n_mile', 'earnings'] 
+out_header = ['date', 'hour', 'drivers_onduty' 't_onduty', 't_occupied', 'n_pass', 'n_trip', 'n_mile', 'earnings']
 
 # initialize variables
 last_key = None
-master_array = []	
+master_array = []
 key = ""
-driver_count = 1
+drivers_onduty = 1
 
 # formates array, converts values from str to floats for indices 4:10
 def format_line(line):
@@ -30,12 +30,12 @@ for line in sys.stdin:
     line = format_line(line)
 
     key = line[0]
-	
+
     # if first entry
     if last_key == None:
         last_key = key
         master_array = line
-	
+
     # if key match
     elif (key == last_key):
         drivers_onduty += 1
@@ -43,8 +43,8 @@ for line in sys.stdin:
         # aggregate all statistics
         for i in range(4 , len(in_header)):
             master_array[i] += line[i]
- 
-   # if new group (hour), print results of previous group 
+
+   # if new group (hour), print results of previous group
     else:
         # string values
         line1 = ('{0}\t{1}\t{2}'.format(master_array[1], master_array[2], str(drivers_onduty)))
@@ -52,7 +52,7 @@ for line in sys.stdin:
         line2 =	('\t'.join(str(master_array[i]) for i in range(4, len(master_array))))
         print(line1 + "\t" + line2)
         # Reset for next group
-        master_array = line	
+        master_array = line
         last_key = key
         drivers_onduty = 1
 
