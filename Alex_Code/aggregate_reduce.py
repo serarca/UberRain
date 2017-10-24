3604a17388a665d09b4ba3240ad3f07ca2a10f53
pyrobluestar <32757427+pyrobluestar@users.noreply.github.com>
2017-10-23 20:38:59 -0700

    Add files via upload

--- Alex_Code/aggregate_reduce.py
+++ Alex_Code/aggregate_reduce.py
@@ -3,9 +3,7 @@
 import sys
 import csv
 
-
-
-in_file = csv.reader(sys.stdin, delimiter=',')
+in_file = csv.reader(sys.stdin, delimiter='\t')
 
 # headers
 in_header = ['key', 'date', 'hour', 'hack', 't_onduty', 't_occupied', 'n_pass', 'n_trip', 'n_mile', 'earnings']
@@ -19,41 +17,48 @@ driver_count = 1
 
 # formates array, converts values from str to floats for indices 4:10
 def format_line(line):
-	formatted = []
-	for i in range(len(in_header)):
-		if(i >= 4):
-			value = float(line.pop(0))
-		else:
-			value = line.pop(0)
-		formatted.append(value)
-	return formatted
+    formatted = []
+    for i in range(len(in_header)):
+        if(i >= 4):
+            value = float(line.pop(0))
+        else:
+            value = line.pop(0)
+        formatted.append(value)
+    return formatted
 
 # streaming
 for line in in_file:
-	key = line[0]
+    key = line[0]
 
-	# formats line
-	line = format_line(line)
+    # formats line
+    line = format_line(line)
 	
-	# if first entry
-	if not last_key:
-		last_key = key
-		master_array = line
+    # if first entry
+    if last_key == None:
+        last_key = key
+        master_array = line
 	
-	# if key match
-	if (key == last_key):
-		driver_count += 1 #adds driver
+    # if key match
+    elif (key == last_key):
+        driver_count += 1
 
-		# aggregate
-		for i in range(4 , len(in_header)):
-			master_array[i] += line[i]
-	# if new group 
-	else:
-		# string values
-		line1 = ('{0},{1},{2}'.format(master_array[1], master_array[2], str(driver_count)))
-		# float values
-		line2 =	(','.join(str(master_array[i]) for i in range(4, len(master_array))))
-		print(line1 + "," + line2)
-		master_array = line	
-		last_key = key
-		driver_count = 1 #reset driver count
+        # aggregate all statistics
+        for i in range(4 , len(in_header)):
+            master_array[i] += line[i]
+ 
+   # if new group (hour), print results of previous group 
+    else:
+        # string values
+        line1 = ('{0}\t{1}\t{2}'.format(master_array[1], master_array[2], str(driver_count)))
+        # float values
+        line2 =	('\t'.join(str(master_array[i]) for i in range(4, len(master_array))))
+        print(line1 + "\t" + line2)
+        # Reset for next group
+        master_array = line	
+        last_key = key
+        driver_count = 1
+
+# print results for final group
+line1 = ('{0}\t{1}\t{2}'.format(master_array[1], master_array[2], str(driver_count)))
+line2 = ('\t'.join(str(master_array[i]) for i in range(4, len(master_array))))
+print(line1 + "\t" + line2)
