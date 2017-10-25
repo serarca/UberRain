10f4b1868d985a488b10c7b1f214d74f91d9d152
Alex Paramo <aparamo@rice16.stanford.edu>
2017-10-25 08:48:13 -0700

    step 5 and 6

--- Alex_Code/join_percip.R
+++ Alex_Code/join_percip.R
@@ -5,34 +5,29 @@ library('tidyverse')
 
 # read in data
 percip_data <- read.csv(file="nyc_precipitation.csv", header=TRUE, sep=",")
-taxi_data <- read.csv(file="test_step6.csv", header=FALSE, sep=",")
+taxi_data <- read.table(file="taxi_jan.txt", header=FALSE, sep='\t')
 
 # set header
 colnames(taxi_data) <- c("date", "hour", "drivers_onduty", "t_onduty", "t_occupied","n_pass", "n_trip", "n_mile", "earnings")
 
-#Prepares percipitation data for merge
-split_dt = data.frame(data=percip_data) 
-split_dt$data.DATE = strptime(data=split_dt$data.DATE, format = '%Y%m%d %H:%M')
-split_dt$data.DATE
-
-
-
-#split_dt$time <- strptime(x = split_dt$time, format="%H:%M")
-#split_dt$time <- as.numeric(split_dt$time, na.rm=TRUE) #not working
-#split_dt$time
-#split_dt$date <- as.Date(split_dt$date, format="%Y%m%d") # works
+#Prepares percipitation data for meirge
+formatted_percip = data.frame(data=percip_data)
+formatted_percip$data.datetime <- as.POSIXct(strptime(formatted_percip$data.DATE, format = "%Y%m%d %H:%M"), tz="UTC")
+formatted_percip$data.DATE <- NULL
 
 # Prepares taxi data for merge
 formatted_taxi <- data.frame(data = taxi_data)
-
-
-#formatted_taxi$date <- as.Date(formatted_taxi$date, format="%m/%d/%Y")
-#formatted_taxi$hour <- as.numeric(formatted_taxi$hour)
-#formatted_taxi$date
-
+formatted_taxi <- unite(formatted_taxi, "data.datetime", c("data.date", "data.hour"), sep= " ")
+formatted_taxi$data.datetime <- as.POSIXct(strptime(formatted_taxi$data.datetime, format="%Y-%m-%d %H"), tz="UTC")
 
 #Merge
-#taxi_percip_data <- left_join(x=formatted_taxi, y=split_dt)
+taxi_percip_data <- left_join(x=formatted_taxi, y=formatted_percip)
 
- 
+#Remove unused columns
+taxi_percip_data$data.STATION <- NULL
+taxi_percip_data$data.STATION_NAME <- NULL
+taxi_percip_data
+
+#write to file
+write.table(taxi_percip_data, file="taxi_rain_merge.tsv", sep='\t')
 
